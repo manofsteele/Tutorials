294bf5134cfc709a91e6cbed08460cfa1d585037
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-11-15 17:06:16 -0800

    Commit before creation of new tutorial folder

--- AA_MERN_tutorial/routes/api/users.js
+++ AA_MERN_tutorial/routes/api/users.js
@@ -1,6 +1,72 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jsonwebtoken = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
+const User = require("../../models/User");
 
 let router = express.Router();
-router.get('/test', (req, res) => res.send('working'));
+router.get("/test", (req, res) => res.send("working"));
+
+router.post("/register", (req, res) => {
+  // Check that no one has registered with this email
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      // Throw a 400 error if address is already taken
+      return res
+        .status(400)
+        .json({ email: "A user has already registered with this address" });
+    } else {
+      // Otherwise create a new user
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "This user does not exist" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = { id: user.id, name: user.name };
+
+        jsonwebtoken.sign(
+          payload,
+          keys.secretOrKey,
+          // Tell the key to expire in one hour
+          { expiresin: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer" + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: "Incorrect password" });
+      }
+    });
+  });
+});
 
 module.exports = router;
